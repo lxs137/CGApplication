22ea90ab9c70662d96152aa1be2e488de22f34ec
lxs137 <lxs137@hotmail.com>
2016-10-18 22:29:49 +0800

    init

---
+++ CGApplication/main.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+
+#define GLEW_STATIC
+#include <gl/glew.h>
+
+#include <GLFW/glfw3.h>
+
+
+const GLchar* vertexShaderSource =
+"#version 330 core\n"
+"layout(location=0) in vec3 position;\n"
+"void main()\n"
+"{gl_Position=vec4(position.x,position.y,position.z,1.0);}";
+
+const GLuint WIDTH = 800, HEIGHT = 600;
+GLfloat vertics[] = {
+	-0.5f, -0.5f, 0.0f,
+	0.5f, 0.5f, 0.0f,
+	0.0f, 0.5f, 0.0f
+};
+void initShader();
+void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode);
+
+// The MAIN function, from here we start the application and run the game loop
+int main()
+{
+	// Init GLFW
+	glfwInit();
+	// Set all the required options for GLFW
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	// Create a GLFWwindow object that we can use for GLFW's functions
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", nullptr, nullptr);
+	if (window == nullptr)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwSetKeyCallback(window, key_callback);
+	glfwMakeContextCurrent(window);
+
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK)
+	{
+		std::cout << "Failed to initialize GLEW" << std::endl;
+		return -1;
+	}
+
+	int width, height;
+	glfwGetFramebufferSize(window, &width, &height);
+	glViewport(0, 0, width, height);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+		glClearColor(0.2f, 0.3f, 0.1f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
+	return 0;
+}
+
+void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode)
+{
+	if (key == GLFW_KEY_ESCAPE&&action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+}
+void initShader()
+{
+	GLuint vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+	GLint success;
+	GLchar infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+	}
+}
+
